6f8f9383028f91e6815e7f4e8fda4286e4cb6553
Nico Bazzoni <you@example.com>
2021-02-06 07:33:51 -0500

    commit tree 925b6b5eead4507ff2810068e4af9ccae31bd66b

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Nico"
+  end 
+
+  get '/hometown' do
+    "My hometown is NYC"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Junky"
+  end
 end
